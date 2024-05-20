fb706a53cf913079efbe6c249f94f0820fb34393
Elena Navarro <elena.navarros@um.es>
2024-05-21 00:37:09 +0200

    weaviate/embeddings.py: construyo y guardo los chunks como embeddings en Weaviate con metadata y usando batch. Mejoro el formato del script para más general

--- weaviate/embeddings.py
+++ weaviate/embeddings.py
@@ -1,64 +1,71 @@
+#!/home/elena/deep-learning-multimodal/weaviate/venvweaviate/bin/python3
 import json
 import weaviate
-import sys
 import os
+import argparse
+from huggingface_hub import login
 from transformers import AutoModel
 
-# Read the write token from the file
-with open("token.txt", "r") as token_file:
-    token_read = token_file.read().strip()
+# Main function
+def main(input_directory, client, model):
 
-# Log in to the Hugging Face Hub using the token
-client = weaviate.Client("http://155.54.95.149:8080", token=token_read)
+    # Get the list of JSON files in the input directory
+    json_files = [f for f in os.listdir(input_directory) if f.endswith('.json')]
+    # Make sure that json_files have already not been populated. 
+    # Otherwise it will insert duplicate records in weaviate.
 
-# If a JSON file is not provided, print an error message and exit
-if len(sys.argv) < 2:
-    print("Usage: ./embeddings.py <json-file>")
-    sys.exit(1) 
-# Get the JSON file name from the command-line arguments   
-json_file = sys.argv[1]
-# Extract the base name of the audio file
-base_name = os.path.basename(json_file)
-# Extract the file name without extension
-file_name = os.path.splitext(base_name)[0]
+    # Process each JSON file
+    for file_name in json_files:
+        # Create the input file path
+        input_file = os.path.join(input_directory, file_name)
+        # Read the JSON file
+        with open(input_file, "r") as f:
+            json_data = json.load(f) 
+        
+        # Extract metadata from the JSON data
+        metadata = json_data["metadata"]
+        
+        with client.batch(batch_size=100) as batch:
+            
+            # Iterate through each chunk in json_data and generate embeddings
+            for chunk in json_data["chunks"]:
+                # Extract text, start time and end time
+                text = chunk["text"]
+                start_time = chunk["start"]
+                end_time = chunk["end"]
 
-# Load JSON file
-try:
-    with open(json_file, "r") as file:
-        data = json.load(file)     
-except Exception as e:  
-    print(f"Error loading JSON file: {e}")
-    sys.exit(1)
+                # Encode the text of the chunk to get the embedding
+                embedding_video = model.encode(text)
 
-# Initialize the model for generating embeddings
-model = AutoModel.from_pretrained('jinaai/jina-embeddings-v2-base-es', trust_remote_code=True)
+                # Create an object to store the chunk in Weaviate
+                data_obj = {
+                    "text": text,
+                    "start": start_time,
+                    "end": end_time,
+                    "metadata": metadata 
+                }
 
+                # Store the object in Weaviate
+                batch.add_data_object(
+                    data_obj,
+                    "Video",
+                    vector=embedding_video
+                )
 
-# METADATA Dhruv ...
+if __name__ == '__main__':
+    # Parse the command-line arguments
+    parser = argparse.ArgumentParser(description='Generate embeddings from chunks.')
+    parser.add_argument('input_directory', type=str, help='Path to the input directory containing JSON files with chunks')
+    args = parser.parse_args()
 
-# Iterate through each chunk in data and generate embeddings
-for chunk in data:
-    # Extract text, start time and end time
-    text = chunk["text"]
-    start_time = chunk["start"]
-    end_time = chunk["end"]
+    # Log in to the Hugging Face Hub using the token
+    login()
+    # Initialize Weaviate client
+    client = weaviate.Client("http://155.54.95.149:8080")
 
-    # SIGUIENDO EL ORDEN DE TESTWEAVIATE.PY + DHRUV
+    # Initialize the model for generating embeddings
+    model = AutoModel.from_pretrained('jinaai/jina-embeddings-v2-base-es', trust_remote_code=True)
+    # trust_remote_code is needed to use the encode method
 
-    # Encode the text of the chunk to get the embedding
-    embedding = model.encode(text)
-
-    # Create an object with the chunk text??? only text???
-    data_obj = {
-        "text": text,
-        "start": start_time,
-        "end": end_time
-        # METADATA hacer como Dhruv que lo obtiene antes
-    }
-
-    # Store the object in Weaviate
-    data_uuid = client.data_object.create(
-        data_obj,
-        "Video",
-        vector=embedding
-    )
+    # Run the extraction
+    main(args.input_directory, client, model)
