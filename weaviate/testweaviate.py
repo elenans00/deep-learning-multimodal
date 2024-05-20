f6737e2a9779bd22c829255f88748ca8654a54b2
Elena Navarro <elena.navarros@um.es>
2024-05-21 00:39:00 +0200

    weaviate/testweaviate.py: prueba de query a Weaviate con el texto 'clase del viernes 29 de abril' para que me devuelva los 10 embeddings más cercanos. Parece prueba superada

--- weaviate/testweaviate.py
+++ weaviate/testweaviate.py
@@ -1,53 +1,38 @@
-## Comprobamos que funciona la conexión con la BBDD
+#!/home/elena/deep-learning-multimodal/weaviate/venvweaviate/bin/python3
+import json
 import weaviate
+import os
+import argparse
 from huggingface_hub import login
-login()
 from transformers import AutoModel
-client = weaviate.Client("http://155.54.95.149:8080")
-client.schema.get()
 
-## Comprobamos que podemos generar los embeddings
-model = AutoModel.from_pretrained('jinaai/jina-embeddings-v2-base-es', trust_remote_code=True) # trust_remote_code is needed to use the encode method
+# Main function
+def main(client, model): #n_records aqui o ver abajo para limite de 10!!
 
-# Creamos objeto con primera frase
-data_obj = {
-    "frase": "Hello from the moon!"
-}
-embedding = model.encode(data_obj["frase"])
+    # Hacemos búsqueda concreta para prueba
+    emb = model.encode("clase del viernes 29 de abril")
 
-data_uuid = client.data_object.create(
-  data_obj,
-  "YourClass",
-  vector=embedding
-)
+    nearVector = {
+        "vector": emb.tolist(),
+    }
 
-# Creamos objeto con la segunda frase
-data_obj2 = {
-    "frase": "I'm an apple!"
-}
-embedding2 = model.encode(data_obj2["frase"])
+    result = (client.query.get("Video", 
+              ["text", "start", "end", "metadata { filename }"])
+              .with_additional("distance")
+              .with_limit(10) # limit the number of records returned
+              .with_near_vector(nearVector).do())
+    
+    print(json.dumps(result, indent=4))
 
-data_uuid2 = client.data_object.create(
-  data_obj2,
-  "YourClass",
-  vector=embedding2
-)
+if __name__ == '__main__':
+    # Log in to the Hugging Face Hub using the token
+    login()
+    # Initialize Weaviate client
+    client = weaviate.Client("http://155.54.95.149:8080")
 
-# Hacemos la búsqueda
-emb3 = model.encode("I'm an apple")
+    # Initialize the model for generating embeddings
+    model = AutoModel.from_pretrained('jinaai/jina-embeddings-v2-base-es', trust_remote_code=True)
+    # trust_remote_code is needed to use the encode method
 
-nearVector = {
-    "vector": emb3.tolist(),
-}
-
-result = (client.query.get("YourClass", "frase").with_additional("distance").with_near_vector(nearVector).do())
-print(result)
-
-# {'data': {'Get': {'YourClass': [{'_additional': {'distance': 0.0750525}, 'frase': "I'm an apple!"}, {'_additional': {'distance': 0.84850264}, 'frase': 'Hello from teh moon!'}]}}}
-
-# Distancia 0 es idéntica. Distancia 1 es opuesta
-# La frase "I'm an apple!" es casi idéntica (0.07 distancia) 
-# La frase "Hello from teh moon!" está muy alejada (0.84)
-
-# HEMOS CAMBIADO EL MODELO POR LO QUE SALEN RESULTADOS SIMILARES Y COHERENTES PERO NO IGUALES
-
+    # Run the extraction
+    main(client, model)
